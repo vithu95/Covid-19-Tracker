39bc54d487ea72d8c82fb6bbe336a3b249037d79
vithu95 <vithu95@hotmail.de>
2020-09-17 15:11:53 +0200

    small bug fix for Total Cases

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -9,7 +9,7 @@ function InfoBox({title,cases,total}) {
         <CardContent>
             <Typography className="infoBox__title">{title}</Typography>
             <h2 className="infoBox__cases">{cases}</h2>
-            <Typography className="infoBox__total">{total}Total</Typography>
+            <Typography className="infoBox__total">{total} Total</Typography>
         </CardContent>
     </Card>
     
